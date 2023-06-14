192525db7d4a8f17e00cc702f9092d3418449190
Daniel Avelar <danielavelar@Daniels-MacBook-Pro.local>
2023-06-14 13:24:57 -0400

    added routes

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const router = express.Router();
+import {
+  getProducts,
+  getProdcutById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+  getTopProducts,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/top', getTopProducts);
+router
+.route('/:id')
+.put(getProdcutById)
+.put(protect, admin, updateProduct)
+.delete(protect, admin, deleteProduct);
+
+export default router;
