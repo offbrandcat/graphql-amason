b0c74500802b6968167f79caa3828f60b4d80c79
offbrandcat <128991168+offbrandcat@users.noreply.github.com>
2023-06-16 10:09:05 -0400

    added more seed data and server config

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1.:27017/amason_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const db = require('./config/db')
 
 dotenv.config();
 
